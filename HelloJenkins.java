e69ace475b0c02e0b59dcb224c557240b6f8c9dc
om24203 <ovchaudhari2006@gmail.com>
2026-09-10 15:07:03 +0530

    Add files via upload

---
+++ HelloJenkins.java
@@ -0,0 +1,28 @@
+public class HelloJenkins {
+
+    public static void main(String[] args) {
+
+        System.out.println("Prime numbers from 1 to 100:");
+
+        for (int number = 2; number <= 100; number++) {
+
+            boolean isPrime = true;
+
+            for (int i = 2; i <= number / 2; i++) {
+
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(number + " ");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Program completed successfully.");
+    }
+}
+
